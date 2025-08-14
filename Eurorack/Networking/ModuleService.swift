007de96d464510fda9eee291aa11177c10307ee8
Mattias Almén <matsmorot@gmail.com>
2025-08-14 13:06:14 +0200

    [REF] "Logging" actual error message

--- Eurorack/Networking/ModuleService.swift
+++ Eurorack/Networking/ModuleService.swift
@@ -14,7 +14,7 @@ class ModuleService {
             let modules = try await FetchClient.shared.fetch(for: [Module].self, endpoint: ModuleEndpoint.getModules)
             return modules
         } catch {
-            print("ModuleService error: Could not fetch modules")
+            print("ModuleService error: Could not fetch modules: \(error.localizedDescription)")
             return [ModuleStore.placeholderModule]
         }
     }
@@ -23,7 +23,7 @@ class ModuleService {
         do {
             return try await FetchClient.shared.fetch(for: Module.self, endpoint: ModuleEndpoint.getModule(id: moduleID))
         } catch {
-            print("ModuleService error: Could not fetch module with ID \(moduleID)")
+            print("ModuleService error: Could not fetch module with ID \(moduleID): \(error.localizedDescription)")
             return ModuleStore.placeholderModule
         }
     }
